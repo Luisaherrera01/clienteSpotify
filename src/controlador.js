9b2d8d99366eae468ac30f6c5d23b57fcba7a509
carolinagraciano <caritocarvajal2012@gmail.com>
2023-04-27 11:56:43 -0500

    se crean las canciones en la pagina

--- src/controlador.js
+++ src/controlador.js
@@ -1,11 +1,34 @@
 import {pedirTokenEnSpotify} from "../services/servicioSpotify.js";
 import { buscarCanciones } from "../services/servicioConsultarCanciones.js";
 
+let fila = document.querySelector("#fila")
+
 pedirTokenEnSpotify()
 .then(function(token){
 
     buscarCanciones(token)
     .then(function(respuesta){
+
+        console.log(fila)
+
+        respuesta.tracks.forEach(function(cancion){
+            let columna = document.createElement("div")
+            columna.classList.add("col")
+
+            let tarjeta = document.createElement("div")
+            tarjeta.classList.add("card","h-100","shadow")
+
+            let pista = document.createElement("audio")
+            pista.classList.add("w-100")
+            pista.setAttribute("controls","controls")
+            pista.src=cancion.preview_url
+
+            tarjeta.appendChild(pista)
+            columna.appendChild(tarjeta)
+            fila.appendChild(columna)
+
+        })
+
         console.log(respuesta.tracks[0])
         console.log(respuesta.tracks[0].name)
         console.log(respuesta.tracks[0].preview_url)
